a011fd338317513f91c360e1e1654e40fece7cc4
Eligio93 <eligio.cristantielli@gmail.com>
2024-05-06 11:09:49 +1000

    creates Message Schema

---
+++ models/message.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const MessageSchema= new Schema({
+    title:{type:String, required:true, minLength: 1},
+    text:{type:String, required:true, minLength:1},
+    author:{type:Schema.Types.ObjectId, ref:'User', required:true}
+})
+
+module.exports=mongoose.model('Message',MessageSchema)
