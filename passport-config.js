24599e2b24a56294d6029ee8d57faba8380a2909
Eligio93 <eligio.cristantielli@gmail.com>
2024-05-07 19:18:17 +1000

    uses email as username

--- passport-config.js
+++ passport-config.js
@@ -3,11 +3,11 @@ const LocalStrategy =require('passport-local').Strategy;
 const User=require('./models/user')
 
 passport.use(
-    new LocalStrategy(async (username, password, done) => {
+    new LocalStrategy({usernameField:'email'},async (username, password, done) => {
       try {
-        const user = await User.findOne({ username: username });
+        const user = await User.findOne({ email: username });
         if (!user) {
-          return done(null, false, { message: "Incorrect username" });
+          return done(null, false, { message: "Incorrect mail" });
         };
         if (user.password !== password) {
           return done(null, false, { message: "Incorrect password" });
