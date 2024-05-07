4053c7644cf71947f061cabda71cbb2338b84b45
Eligio93 <eligio.cristantielli@gmail.com>
2024-05-07 21:03:37 +1000

    put serializing functions at the top

--- passport-config.js
+++ passport-config.js
@@ -1,35 +1,41 @@
 const passport = require('passport')
-const LocalStrategy =require('passport-local').Strategy;
-const User=require('./models/user')
+const LocalStrategy = require('passport-local').Strategy;
+const User = require('./models/user')
 
-passport.use(
-    new LocalStrategy({usernameField:'email'},async (username, password, done) => {
-      try {
-        const user = await User.findOne({ email: username });
-        if (!user) {
-          return done(null, false, { message: "Incorrect mail" });
-        };
-        if (user.password !== password) {
-          return done(null, false, { message: "Incorrect password" });
-        };
-        return done(null, user);
-      } catch(err) {
-        return done(err);
-      };
-    })
-  );
-  passport.serializeUser((user, done) => {
+
+
+passport.serializeUser((user, done) => {
     done(null, user.id);
-  });
-  
-  passport.deserializeUser(async (id, done) => {
+});
+
+passport.deserializeUser(async (id, done) => {
     try {
       const user = await User.findById(id);
+      console.log(user)
       done(null, user);
     } catch(err) {
       done(err);
     };
-});
+  });
 
-module.exports=passport
+passport.use(
+    new LocalStrategy({ usernameField: 'email' }, async (username, password, done) => {
+        try {
+            const user = await User.findOne({ email: username });
+            console.log(user)
+            if (!user) {
+                return done(null, false, { message: "Incorrect mail" });
+            };
+            if (user.password !== password) {
+                return done(null, false, { message: "Incorrect password" });
+            };
+            return done(null, user);
+        } catch (err) {
+            return done(err);
+        };
+    })
+);
+
+
+module.exports = passport
 
