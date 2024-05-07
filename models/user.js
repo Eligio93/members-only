a7cb149c9981395186b1816cb6b5d94b988e5b1c
Eligio93 <eligio.cristantielli@gmail.com>
2024-05-07 19:18:38 +1000

    temporry changes on schema requirements

--- models/user.js
+++ models/user.js
@@ -4,8 +4,8 @@ const Schema = mongoose.Schema;
 const UserSchema= new Schema({
     name:{type:String, required:true, minLength: 1},
     lastName:{type:String, required:true, minLength:1},
-    email:{type:String, required:true, minLength:5},
-    password:{type:String, required:true, minLength:5},
+    email:{type:String, required:true, minLength:1},
+    password:{type:String, required:true, minLength:1},
     member:{type:Boolean, required:true},
     admin:{type:Boolean,required:true}
 })
