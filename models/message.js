898863bb34b1cb77c793b51ea1a4f0c5481ce629
Eligio93 <eligio.cristantielli@gmail.com>
2024-05-08 19:10:18 +1000

    adds date to the message Schema

--- models/message.js
+++ models/message.js
@@ -4,7 +4,8 @@ const Schema = mongoose.Schema;
 const MessageSchema= new Schema({
     title:{type:String, required:true, minLength: 1},
     text:{type:String, required:true, minLength:1},
-    author:{type:Schema.Types.ObjectId, ref:'User', required:true}
+    author:{type:Schema.Types.ObjectId, ref:'User', required:true},
+    date:{type:Date, required:true}
 })
 
 module.exports=mongoose.model('Message',MessageSchema)
