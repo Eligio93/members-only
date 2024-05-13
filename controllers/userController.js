eebcbc2d73012e3013d0f8c7a2a0f7713fcfd276
Eligio93 <eligio.cristantielli@gmail.com>
2024-05-13 13:42:15 +1000

    defines upgrade get and post requests

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,6 +5,23 @@ const { body, validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs')
 require('dotenv').config()
 
+
+function checkAdmin(key){
+  if(key===process.env.ADMIN_KEY){
+    return true
+  }
+  return false
+}
+function checkMember(key){
+  if(checkAdmin(key) || key=== process.env.MEMBER_KEY){
+    return true
+  }
+  return false
+}
+
+
+
+
 /*GET login*/
 exports.login_get = function (req, res, next) {
   const logInMessage=req.session.messages
@@ -37,21 +54,7 @@ exports.signup_get = function (req, res) {
 exports.signup_post = [
   //insert validation and sanitizing
   asyncHandler(async (req, res, next) => {
-    function checkAdmin(key){
-      if(key===process.env.ADMIN_KEY){
-        return true
-      }
-      return false
-    }
-    function checkMember(key){
-      if(checkAdmin(key) || key=== process.env.MEMBER_KEY){
-        return true
-      }
-      return false
-    }
-
-
-    
+   
     const hashedPassword = await bcrypt.hash(req.body.password, 10);
     try {
       const user = new User({
@@ -70,3 +73,17 @@ exports.signup_post = [
   })
 ]
 
+/*GEt Upgrade*/
+exports.upgrade_get= (req,res,next)=>{
+  res.render('upgrade')
+}
+
+/*POST Upgrade*/
+exports.upgrade_post=asyncHandler(async (req,res,next)=>{
+  if(req.body['upgrade-key'] === process.env.ADMIN_KEY || req.body['upgrade-key'] === process.env.MEMBER_KEY ){
+    await User.findByIdAndUpdate(req.user.id,{admin:checkAdmin(req.body['upgrade-key']), member:checkMember(req.body['upgrade-key'])},{});
+    res.redirect('/')
+  }else{
+    res.render('upgrade',{message:'Insert a valid Key'})
+  }  
+})
