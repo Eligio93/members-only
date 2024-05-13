bc5a85c32e68c8c354a9deb4d89d6fabdbd3d20e
Eligio93 <eligio.cristantielli@gmail.com>
2024-05-13 17:37:24 +1000

    defines lengths on User Schema datas

--- models/user.js
+++ models/user.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const UserSchema= new Schema({
-    name:{type:String, required:true, minLength: 1},
-    lastName:{type:String, required:true, minLength:1},
-    email:{type:String, required:true, minLength:1},
-    password:{type:String, required:true, minLength:1},
+    name:{type:String, required:true, minLength: 3},
+    lastName:{type:String, required:true, minLength:3},
+    email:{type:String, required:true},
+    password:{type:String, required:true, minLength:4},
     member:{type:Boolean, required:true},
     admin:{type:Boolean,required:true}
 })
